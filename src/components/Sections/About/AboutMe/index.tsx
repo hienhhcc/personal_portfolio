578b28682d11dd3291699952819700f8d75c345c
vuvinhhien <hien.vuvinh@hdwebsoft.dev>
2022-12-22 13:50:57 +0700

    refactor about me component

--- src/components/Sections/About/AboutMe/index.tsx
+++ src/components/Sections/About/AboutMe/index.tsx
@@ -1,9 +1,14 @@
 import { FaCaretRight } from "react-icons/fa";
 import { GrDocumentText } from "react-icons/gr";
-
-import { LineBreak } from "../../..";
+import LineBreak from "src/components/LineBreak";
 
 import classes from "./styles/AboutMe.module.scss";
+
+const aboutMe = [
+  "Mình là một web developer, học tập tại trường ĐH Khoa Học Tự Nhiên, hiện đang sinh sống tại TPHCM.",
+  "Có khả năng làm việc nhóm tốt, luôn cố gắng tiếp thu, tìm tòi, học hỏi cái mới.",
+  "Ngoài ra mình cũng đam mê các trò chơi điện tử, đặc biệt là các blockchain game.",
+];
 
 const AboutMe = () => {
   return (
@@ -14,34 +19,15 @@ const AboutMe = () => {
       </div>
       <div className={classes.rightContent}>
         <ul>
-          <li>
-            <FaCaretRight className={classes.icon} />
-            <span>
-              Mình là một web developer, học tập tại trường ĐH Khoa Học Tự
-              Nhiên, hiện đang sinh sống tại TPHCM.
-            </span>
-          </li>
-          <li>
-            <FaCaretRight className={classes.icon} />
-            <span>
-              Có khả năng làm việc nhóm tốt, luôn cố gắng tiếp thu, tìm tòi, học
-              hỏi cái mới.
-            </span>
-          </li>
-          <li>
-            <FaCaretRight className={classes.icon} />
-            <span>
-              Ngoài ra mình cũng đam mê các trò chơi điện tử, đặc biệt là các
-              blockchain game.
-            </span>
-          </li>
+          {aboutMe.map((about) => (
+            <li key={about}>
+              <FaCaretRight className={classes.icon} />
+              <span>{about}</span>
+            </li>
+          ))}
         </ul>
         <div className={classes.action}>
-          <a
-            href="/files/vuvinhhien-cv-Eng.pdf"
-            download
-            className={classes.downloadCVButton}
-          >
+          <a href='/files/vuvinhhien-cv-Eng.pdf' download className={classes.downloadCVButton}>
             <GrDocumentText />
             <span>Tải CV</span>
           </a>
